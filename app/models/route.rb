f2fd2584668b0ece8892bd446afc541c03f0d77a
A3knok <a3-knok113@docomo.ne.jp>
2025-12-01 18:21:02 +0900

    add:投稿編集機能の実装

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,8 +1,10 @@
 class Route < ApplicationRecord
   belongs_to :user
-  belongs_to :gate
-  belongs_to :exit
+  belongs_to :gate, optional: true # 自動バリデーションを無効化
+  belongs_to :exit, optional: true # 自動バリデーションを無効化
 
+  validates :gate_id, presence: true
+  validates :exit_id, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 1000 }
   validates :estimated_time, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
