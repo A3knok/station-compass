036378391fad0c7bc02aa635f4d0ea4e7febc43b
國岡亜未 <ami@Macbook-Air-3.local>
2025-12-31 12:37:20 +0900

    wip:モデルスペック途中

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,7 +37,38 @@ RSpec.describe User, type: :model do
         expect(user.errors[:email]).to include("すでに使用されています")
       end
 
-      
+      it "新規作成時パスワードがない場合に無効であること" do
+        user.password = nil
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+
+      it "パスワードが6文字未満の場合に無効であること" do
+        user.password = "12345"
+        user.password_confirmation = "12345"
+        expect(user).to be_invalid
+        expect(user.errors[:password]).to include("は6文字以上で入力してください")
+      end
+
+      it "パスワード(確認用)がない場合に無効であること" do
+        user.password_confirmation = nil
+        expect(user).to be_invalid
+        expect(user.errors[:password_confirmation]).to include("を再入力してください")
+      end
+
+      it "パスワードとパスワード(確認用)が一致しない場合に無効であること" do
+        user.password = "password123"
+        user.password_confirmation = "defferent123"
+        expect(user).to be_invalid
+        expect(user.errors[:password_confirmation]).to include("パスワードの入力が一致しません")
+      end
+
+      it "一般ユーザーがゲストユーザーのメールアドレスを使用できないこと" do
+        user.email = "guest@example.com"
+        user.guest = false
+        expect(user).to be_invalid
+        expect(user.errors[:email]).to include("は使用できません")
+      end
     end
   end
 end
