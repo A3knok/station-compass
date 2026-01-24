b2a3285a66f601478f4d9eef3e6de49f7e16d305
國岡亜未 <ami@Macbook-Air-3.local>
2026-01-24 18:12:11 +0900

    route_searchブランチに作業内容を反映

--- app/javascript/controllers/slim_select_controller.js
+++ app/javascript/controllers/slim_select_controller.js
@@ -5,15 +5,32 @@ export default class extends Controller {
   connect() {
     console.log('Slim Select initializing...', this.element)
 
-    new SlimSelect({
+    if (this.slimSelect) {
+      console.log('Already initialized')
+      return
+    }
+
+    // slim-select v2の初期化方法
+    this.slimSelect = new SlimSelect({
       select: this.element,
-      settings: {
-        placeholderText: "タグを選択",
-        searchPlaceholder: "タグを検索",
-        searchText: "該当なし"
-      }
+      placeholder: "タグを選択",
+      searchPlaceholder: "タグを検索",
+      searchText: "該当なし",
+      allowDeselect: true,
+      hideSelectedOption: true, // 追加: 選択済みオプションを隠す
     })
 
+    // 初期化後に元のselectタグを強制的に非表示
+    this.element.style.display = 'none'
+    this.element.style.visibility = 'hidden'
+
     console.log('Slim Select initialized!')
+  }
+
+  disconnect() {
+    if (this.slimSelect) {
+      this.slimSelect.destroy()
+      this.slimSelect = null
+    }
   }
 }
