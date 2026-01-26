0cb2bccb5abeb42b866b215882934d47568f551e
A3knok <a3-knok113@docomo.ne.jp>
2026-01-26 17:50:35 +0900

    fix:stimulusの設定修正

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,5 @@ class Station < ApplicationRecord
   has_many :exits, dependent: :destroy
   has_many :gates, dependent: :destroy
   has_many :routes, through: :gates
+  has_many :railway_companies, through: :gates
 end
