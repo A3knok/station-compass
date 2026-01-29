6183a54c2a8f5ffa2ee63aa0aea3de4471096312
A3knok <a3-knok113@docomo.ne.jp>
2026-01-29 15:14:37 +0900

    add:駅のルート一覧ページにmap表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_20_070707) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_28_035335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -81,6 +81,8 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_20_070707) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "latitude", precision: 10, scale: 6
+    t.decimal "longitude", precision: 10, scale: 6
     t.index ["id"], name: "index_stations_on_id", unique: true
   end
 
