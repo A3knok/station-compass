6e7a3a21b6a550cecceab6f6bc1af1e0a2749c7c
A3knok <a3-knok113@docomo.ne.jp>
2026-02-02 16:46:31 +0900

    fix:rubocop修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
   skip_before_action :authenticate_user!, only: %i[new create]
   skip_before_action :check_guest_user, only: %i[new create]
-  
+
   def after_sign_up_path_for(resource)
     user_path(resource)
   end
