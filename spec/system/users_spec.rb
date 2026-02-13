7b5eb7e16a02a448ec484d3037b9aac8484cdd98
A3knok <a3-knok113@docomo.ne.jp>
2026-02-13 11:31:01 +0900

    users_specの作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
-  let!(:user) { create(:user) }
-
   describe "ログイン前" do
     describe "ユーザー新規登録" do
       context "フォームの入力値が正常" do
@@ -96,6 +94,10 @@ RSpec.describe "Users", type: :system do
     end
 
     describe "パスワードリセット" do
+      before do
+        ActionMailer::Base.deliveries.clear
+      end
+
       context "登録済みのメールアドレスを入力した場合" do
         it "パスワードリセットメールが送信される" do
           created_user = create(:user)
@@ -103,23 +105,53 @@ RSpec.describe "Users", type: :system do
           fill_in "メールアドレス", with: created_user.email
           click_button "送信"
 
+          expect(page).to have_content("パスワードリセット手順を送信しました", wait: 10)
           expect(current_path).to eq new_user_session_path
+
+          expect(ActionMailer::Base.deliveries.size).to eq 1
+          expect(ActionMailer::Base.deliveries.last.to).to eq [created_user.email]
         end
       end
     end
   end
 
   describe "ログイン後" do
+    let!(:user) { create(:user) }
+
     before { login_as(user) }
 
-    describe "ユーザー編集" do
-      it "ユーザーを編集できる" do
-        visit edit_user_registration_path
-        fill_in "メールアドレス", with: "updated@example.com"
-        fill_in "現在のパスワード", with: "password123"
-        click_button "更新"
+    describe "アカウント情報編集" do
+      context "入力値が正常" do
+        it "アカウント情報を編集できる" do
+          visit edit_user_registration_path
+          fill_in "メールアドレス", with: "updated@example.com"
+          fill_in "現在のパスワード", with: "password123"
+          click_button "更新"
 
-        expect(page).to have_content("アカウント情報を変更しました")
+          expect(page).to have_content("アカウント情報を変更しました")
+        end  
+      end
+
+      context "現在のパスワードが未入力の場合" do
+        it "アカウント情報の更新に失敗する" do
+          visit edit_user_registration_path
+          fill_in "メールアドレス", with: "updated@example.com"
+          fill_in "現在のパスワード", with: ""
+          click_button "更新"
+
+          expect(page).to have_content("現在のパスワードを入力してください")
+        end
+      end
+
+      context "現在のパスワードが間違っている場合" do
+        it "アカウント情報の更新に失敗する" do
+          visit edit_user_registration_path
+          fill_in "メールアドレス", with: "updated@example.com"
+          fill_in "現在のパスワード", with: "wrong_password"
+          click_button "更新"
+
+          expect(page).to have_content("現在のパスワードが正しくありません")
+        end
       end
     end
   end
