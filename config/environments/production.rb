ad5cc31016c27c88e89755caa083d976983c3b40
A3knok <a3-knok113@docomo.ne.jp>
2026-02-24 17:49:10 +0900

    fix:デバッグ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # メール送信の設定（SendGrid）
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # SendGrid SMTP設定
   config.action_mailer.smtp_settings = {
