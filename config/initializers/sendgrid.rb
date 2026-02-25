65ca01b6b536de8309b5b815432a754d69fc9a52
A3knok <a3-knok113@docomo.ne.jp>
2026-02-25 12:03:04 +0900

    SendGrid::MailInterceptorの削除(web API利用では不要)

--- config/initializers/sendgrid.rb
+++ config/initializers/sendgrid.rb
@@ -1,4 +1,2 @@
-ActionMailer::Base.register_interceptor(SendGrid::MailInterceptor)
-
 ActionMailer::Base.add_delivery_method :sendgrid_actionmailer, Mail::SendGrid,
   api_key: Rails.application.credentials.dig(:sendgrid, :api_key)
