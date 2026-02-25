8143daa2963e8db68f5f5670a277bcd92c94a6e4
A3knok <a3-knok113@docomo.ne.jp>
2026-02-25 14:18:20 +0900

    fix:productionの設定を1つのファイルで管理

--- config/initializers/sendgrid.rb
+++
@@ -1,6 +0,0 @@
-if Rails.env.production?
-  require "sendgrid-actionmailer"
-
-  ActionMailer::Base.add_delivery_method :sendgrid_actionmailer, Mail::SendGrid,
-    api_key: Rails.application.credentials.dig(:sendgrid, :api_key)
-end
